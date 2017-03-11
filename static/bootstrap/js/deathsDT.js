7f6a31a4b973633c77a7f95951efafb013e61940
Greg <zomgreg@gmail.com>
2017-03-11 13:52:53 -0800

    update

---
+++ static/bootstrap/js/deathsDT.js
@@ -0,0 +1,14 @@
+/**
+ * Created by moselleg on 12/28/16.
+ */
+$('#stage_deaths').DataTable( {
+    "order": [[ 0, "desc" ]]
+} );
+
+$('#environment_deaths').DataTable( {
+    "order": [[ 0, "desc" ]]
+} );
+
+$('#enemy_deaths').DataTable( {
+    "order": [[ 0, "desc" ]]
+} );
